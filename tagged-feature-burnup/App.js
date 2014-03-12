b5258f490133d6dbf30fb4df9cf917ff28e60045
Barry Mullan <mullan@gmail.com>
2014-03-12 15:33:11 -0400

    Tagged

--- tagged-feature-burnup/App.js
+++ tagged-feature-burnup/App.js
@@ -3,7 +3,7 @@ var app = null;
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+    items:{ },
 
     launch: function() {
 
